94a26e759fda61429394041a92fb8f384e2f7127
morshedul-antor <antor.morshedul@gmail.com>
2022-10-08 12:09:17 +0600

    fix: extra code remove

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
-import Body from './Body/Body';
-import Navbar from './Navbar/Navbar';
-import Sidebar from './Sidebar/Sidebar';
-import classes from './Layout.module.css';
-
-//import bar from './/bars.png';
-import About from './About/About';
+import React from "react";
+import Body from "./Body/Body";
+import Navbar from "./Navbar/Navbar";
+import classes from "./Layout.module.css";
+import About from "./About/About";
 
 const Layout = () => {
-    const [show, setShow] = useState(true);
-    // const handleButton = () => {
-    //     setShow(!show);
-    //     console.log(show);
-    //  }
-    return (
-        <div className={classes.container}>
-            <Navbar />
-           
-            {/* <Sidebar show={show} setShow={setShow}/> */}
-            
-            <Body show={show} />
-
-            <About show={show} />
-        </div>
-    );
+  return (
+    <div className={classes.container}>
+      <Navbar />
+      {/* <Sidebar show={show} setShow={setShow}/> */}
+      <Body />
+      <About />
+    </div>
+  );
 };
 
 export default Layout;
